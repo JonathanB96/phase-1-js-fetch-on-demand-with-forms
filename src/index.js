21d68c77aa7cfa6bc1d9911e0fc6fd7347e6d650
jonathan bola <bolajonathan@yahoo.com>
2021-12-23 09:28:42 -0600

    fetch with form

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 const init = () => {
+    
+    
+    const inputForm = document.querySelector('form')
+    inputForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    let input = document.querySelector("input#searchByID").value
+    fetch(`http://localhost:3000/movies/${input}`)
+    .then(res => res.json())
+    .then(json =>{
+        const title = document.querySelector('section#movieDetails h4')
+        const summary = document.querySelector('section#movieDetails p')
+        title.innerText = json.title
+        summary.innerText = json.summary})
+
+        inputForm.reset()
+    
+        
+        });
+    
+    
   
 }
 
-document.addEventListener('DOMContentLoaded', init);
+
+
+document.addEventListener('DOMContentLoaded', init); 
